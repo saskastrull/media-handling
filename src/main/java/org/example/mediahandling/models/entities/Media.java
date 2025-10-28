9685cda3966201cb6c52811216c11991431b5426
Ann-Louis Lilja <lilja.annlouis@gmail.com>
2025-10-28 15:41:00 +0100

    chore: Add id attribute, getter and setter.

--- src/main/java/org/example/mediahandling/models/entities/Media.java
+++ src/main/java/org/example/mediahandling/models/entities/Media.java
@@ -1,4 +1,22 @@
 package org.example.mediahandling.models.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Media {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
