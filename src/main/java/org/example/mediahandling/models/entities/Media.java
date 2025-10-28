b26701df13681b41fcce82f96c3314e5ac788b5d
Ann-Louis Lilja <lilja.annlouis@gmail.com>
2025-10-28 16:08:55 +0100

    chore: Add more attributes in media entity class.

--- src/main/java/org/example/mediahandling/models/entities/Media.java
+++ src/main/java/org/example/mediahandling/models/entities/Media.java
@@ -1,22 +1,46 @@
 package org.example.mediahandling.models.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
+@Table(name = "media")
 public class Media {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long mediaId;
 
-    public void setId(Long id) {
-        this.id = id;
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 50, nullable = false)
+    private String genre;
+
+    @Column(length = 50, nullable = false)
+    private LocalDate releaseDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "artist_id", nullable = false)
+    private Artist artist;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "media_type_id", nullable = false)
+    private MediaType mediaType;
+
+    @ManyToMany(fetch = FetchType.LAZY) //There can be many medias in one album or many albums
+    @JoinColumn(name = "album_id")
+    private List<Album> albums;
+
+    public Media() {}
+
+    public Long getMediaId() {
+        return mediaId;
     }
 
-    public Long getId() {
-        return id;
+    public void setMediaId(Long mediaId) {
+        this.mediaId = mediaId;
     }
 }
