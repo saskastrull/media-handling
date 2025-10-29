13d1175f1cb83b03ae59262d6068198044e0558c
Ann-Louis Lilja <lilja.annlouis@gmail.com>
2025-10-29 20:49:46 +0100

    feature: Added attributes for MediaType entity, getters, setters, constructor and spring.sql.init.mode=never in application-dev.properties to test h2-database with empty data.sql.

--- src/main/java/org/example/mediahandling/models/entities/MediaType.java
+++ src/main/java/org/example/mediahandling/models/entities/MediaType.java
@@ -1,4 +1,33 @@
 package org.example.mediahandling.models.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "media_type")
 public class MediaType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mediaTypeId;
+
+    @Column(length = 30, unique = true, nullable = false)
+    private String type;
+
+    public MediaType() {}
+
+    public Long getMediaTypeId() {
+        return mediaTypeId;
+    }
+
+    public void setMediaTypeId(Long mediaTypeId) {
+        this.mediaTypeId = mediaTypeId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
